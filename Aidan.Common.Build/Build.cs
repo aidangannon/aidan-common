0a9fbba6475a29cc5c363e9142c81dde352b06ee
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-15 20:17:54 +0100

    added pack pipeline

--- Aidan.Common.Build/Build.cs
+++ Aidan.Common.Build/Build.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
@@ -15,7 +19,25 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    static readonly string[] Libraries;
+
+    static readonly string NugetSource;
+    static readonly string NugetApiKey;
+
     [Solution] readonly Solution Solution;
+
+    static Build( )
+    {
+        NugetSource = Environment.GetEnvironmentVariable( "NUGET_SOURCE" );
+        NugetApiKey = Environment.GetEnvironmentVariable( "NUGET_API_KEY" );
+        const string rootNamespace = "Aidan.Common";
+        Libraries = new [ ]
+        {
+            $"{rootNamespace}.Core",
+            $"{rootNamespace}.Utils",
+            $"{rootNamespace}.DependencyInjection"
+        };
+    }
 
     Target Clean => _ => _
         .Before(Restore)
@@ -38,5 +60,53 @@ class Build : NukeBuild
                 .SetProjectFile(Solution)
                 .EnableNoRestore( ) );
         });
+
+    Target Pack => _ => _
+        .Executes( ( ) =>
+        {
+            foreach( var library in Libraries )
+            {
+                if( HasChanged( library ) )
+                {
+                    var version = GetLatestVersion( library );
+                    var versionParts = version
+                        .Split( "." )
+                        .Select( x => int.Parse( x ) )
+                        .ToArray( );
+                    var (majorVersion, minorVersion, patchVersion) =
+                        ( versionParts[ 0 ], versionParts[ 1 ], versionParts[ 2 ] );
+                    var newVersion = $"{majorVersion}.{minorVersion}.{patchVersion + 1}";
+                    DotNetPack( s => s
+                        .SetProject( Solution.GetProject( library ) )
+                        .SetConfiguration( Configuration.Release )
+                        .SetVersion( newVersion ) );
+                    DotNetNuGetPush( s => s
+                        .SetSource( NugetSource )
+                        .SetApiKey( NugetApiKey )
+                        .SetTargetPath( $"{RootDirectory}\\{library}\\bin\\Release\\{library}.{newVersion}.nupkg" ) );
+                }
+            }
+        } );
+
+    private bool HasChanged( string libName )
+    {
+        var process = ProcessTasks.StartProcess( "git", "status .", $"{RootDirectory}\\{libName}" );
+        process.WaitForExit(  );
+        return process
+            .Output
+            .Count > 5;
+    }
+    
+    private string GetLatestVersion( string libName )
+    {
+        var process = ProcessTasks.StartProcess( "dotnet", $"search {libName}" );
+        process.WaitForExit(  );
+        return new Regex( @"[0-9].[0-9].[0-9]" )
+            .Match( process
+                .Output
+                .Select( x => x.Text )
+                .ToArray(  )[ 2 ] )
+            .ToString( );;
+    }
 
 }
