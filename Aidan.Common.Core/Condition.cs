210df6db248853a6786ba53ec0eca535bb9f47c1
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-17 22:18:33 +0100

    testing changes pre-commit

--- Aidan.Common.Core/Condition.cs
+++ Aidan.Common.Core/Condition.cs
@@ -4,7 +4,6 @@ namespace Aidan.Common.Core
 {
     public abstract class Condition<T> where T : Exception, new( )
     {
-        
         public void Evaluate( bool predicate )
         {
             if( !predicate ) throw new T( );
