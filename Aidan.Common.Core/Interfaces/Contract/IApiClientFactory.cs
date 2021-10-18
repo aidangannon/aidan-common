b32ce89a1eb8bcfab94c6af2e71212be1ef0fbae
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-18 20:13:47 +0100

    made serialization more generic

--- Aidan.Common.Core/Interfaces/Contract/IApiClientFactory.cs
+++ Aidan.Common.Core/Interfaces/Contract/IApiClientFactory.cs
@@ -1,8 +1,10 @@
-﻿using Aidan.Common.Core.Interfaces.Excluded;
+﻿using System;
+using Aidan.Common.Core.Interfaces.Excluded;
 
 namespace Aidan.Common.Core.Interfaces.Contract
 {
     public interface IApiClientFactory : IFactory
     {
+        IApiClient<T> Factory<T>( Uri uri, string token ) where T : ISerializer;
     }
 }
