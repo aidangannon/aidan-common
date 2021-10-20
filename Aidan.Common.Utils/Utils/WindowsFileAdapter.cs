9a99622e709451ba9ad253621c3d9ff9699a3f26
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-20 18:59:19 +0100

    added file adapter

---
+++ Aidan.Common.Utils/Utils/WindowsFileAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using Aidan.Common.Core;
+using Aidan.Common.Core.Interfaces.Contract;
+
+namespace Aidan.Common.Utils.Utils
+{
+    public class WindowsFileAdapter : IFileAdapter
+    {
+        public Result Exists( string path ) =>
+            File.Exists( path ) ? Result.Success( ) : Result.Error( $"file at {path} was not found" ) ;
+    }
+}
