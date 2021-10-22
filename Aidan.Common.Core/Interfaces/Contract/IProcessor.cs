bf0a3271600c89e983fba6df40e8417ec8addd8a
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-22 01:13:31 +0100

    add process object - for running cmds

---
+++ Aidan.Common.Core/Interfaces/Contract/IProcessor.cs
@@ -0,0 +1,7 @@
+﻿namespace Aidan.Common.Core.Interfaces.Contract
+{
+    public interface IProcessor
+    {
+        Result RunAndWait( string process, string args );
+    }
+}
---
+++ Aidan.Common.Utils/Utils/Processor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using Aidan.Common.Core;
+using Aidan.Common.Core.Interfaces.Contract;
+
+namespace Aidan.Common.Utils.Utils
+{
+    public class Processor : IProcessor
+    {
+        public Result RunAndWait( string process, string args )
+        {
+            var processObj = new Process();
+            var startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = process,
+                Arguments = args
+            };
+            processObj.StartInfo = startInfo;
+            try
+            {
+                processObj.Start( );
+                processObj.WaitForExit( );
+            }
+            catch ( Exception ex ) when (
+                ex is InvalidOperationException
+                || ex is Win32Exception
+                || ex is PlatformNotSupportedException
+                )
+            {
+                return Result.Error( ex.Message );
+            }
+
+            if( processObj.ExitCode == 1 )
+            {
+                return Result.Success( );
+            }
+            else
+            {
+                return Result.Error( $"there was an error in running execution :P {processObj.StandardOutput.ReadToEnd( )}" );
+            }
+        }
+    }
+}
