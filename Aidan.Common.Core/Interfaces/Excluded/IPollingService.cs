e7f16c82225c543e68fbc3c5fbeca100e5c5ba03
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-06 02:24:24 +0000

    made polling service a singleton

--- Aidan.Common.Core/Interfaces/Excluded/IPollingService.cs
+++ Aidan.Common.Core/Interfaces/Excluded/IPollingService.cs
@@ -1,5 +1,9 @@
-﻿namespace Aidan.Common.Core.Interfaces.Excluded
+﻿using Aidan.Common.Core.Attributes;
+using Aidan.Common.Core.Enum;
+
+namespace Aidan.Common.Core.Interfaces.Excluded
 {
+    [ Service( Scope = ServiceLifetimeEnum.Singleton ) ]
     public interface IPollingService : IInitialisable
     {
         void Cancel( );
